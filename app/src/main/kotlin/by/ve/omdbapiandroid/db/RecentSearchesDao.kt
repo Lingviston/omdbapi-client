dc01e8d98f2696b248b6b87d9cb5ab33add81a4a
Uladzislau Yarmolin <Uladzislau_Yarmolin@epam.com>
2019-06-08 18:19:52 +0300

    - Order recent searches by uid.

--- app/src/main/kotlin/by/ve/omdbapiandroid/db/RecentSearchesDao.kt
+++ app/src/main/kotlin/by/ve/omdbapiandroid/db/RecentSearchesDao.kt
@@ -9,7 +9,7 @@ import androidx.room.Query
 @Dao
 interface RecentSearchesDao {
 
-    @Query("SELECT * from RecentSearches")
+    @Query("SELECT * from RecentSearches ORDER BY uid DESC")
     fun getAll(): DataSource.Factory<Int, RecentSearchEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
