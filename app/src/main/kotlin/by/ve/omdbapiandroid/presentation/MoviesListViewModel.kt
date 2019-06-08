ed6695a974c1ae6afd7cf180db4328badf8c6519
Uladzislau Yarmolin <Uladzislau_Yarmolin@epam.com>
2019-06-08 14:51:55 +0300

    - Implemented search input support.

--- app/src/main/kotlin/by/ve/omdbapiandroid/presentation/MoviesListViewModel.kt
+++ app/src/main/kotlin/by/ve/omdbapiandroid/presentation/MoviesListViewModel.kt
@@ -1,14 +1,15 @@
 package by.ve.omdbapiandroid.presentation
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import by.ve.omdbapiandroid.domain.MoviesDataSourceFactory
 import by.ve.omdbapiandroid.view.MovieAdapterItem
-import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -17,6 +18,10 @@ class MoviesListViewModel @Inject constructor(
 ) : ViewModel() {
 
     val movies: LiveData<PagedList<MovieAdapterItem>>
+
+    val query = MutableLiveData<String>("")
+
+    private val querySubject = PublishSubject.create<String>()
 
     private val compositeDisposable = CompositeDisposable()
 
@@ -28,13 +33,24 @@ class MoviesListViewModel @Inject constructor(
             .setEnablePlaceholders(false)
             .build()
 
-        moviesDataSourceFactory.query = "Home Alone"
         movies = LivePagedListBuilder<Int, MovieAdapterItem>(moviesDataSourceFactory.map {
             MovieAdapterItem(it.title, it.year, it.posterUrl)
         }, moviesListConfig).build()
 
-        Single.just("Spider-man").delay(5, TimeUnit.SECONDS).subscribe { query ->
-            moviesDataSourceFactory.query = query
-        }
+        startListeningSearchQuery()
+    }
+
+    fun onSearchQueryChange(newQuery: String) {
+        query.value = newQuery
+        querySubject.onNext(newQuery)
+    }
+
+    private fun startListeningSearchQuery() {
+        compositeDisposable += querySubject
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .subscribe { query ->
+                moviesDataSourceFactory.query = query
+            }
     }
 }
--- app/src/main/kotlin/by/ve/omdbapiandroid/view/BindingAdapters.kt
+++ app/src/main/kotlin/by/ve/omdbapiandroid/view/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package by.ve.omdbapiandroid.view
 
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -28,4 +29,31 @@ fun RecyclerView.itemSpacing(spacing: Float?) {
         ViewCompat.setPaddingRelative(this, spacingInt, spacingInt, spacingInt, spacingInt)
         addItemDecoration(ItemOffsetDecoration(spacingInt))
     }
+}
+
+@BindingAdapter(value = ["query"])
+fun SearchView.query(query: String?) {
+    if (query != null && this.query != query) {
+        setQuery(query, false)
+    }
+}
+
+@BindingAdapter(value = ["onQueryChange","onQuerySubmit"], requireAll = false)
+fun SearchView.onQueryChanged(onQueryChange: BindingAction<String>?, onQuerySubmit: BindingAction<String>?) {
+    setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        override fun onQueryTextSubmit(query: String): Boolean {
+            onQuerySubmit?.invoke(query)
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String): Boolean {
+            onQueryChange?.invoke(newText)
+            return false
+        }
+    })
+}
+
+interface BindingAction<T> {
+
+    fun invoke(value: T)
 }
